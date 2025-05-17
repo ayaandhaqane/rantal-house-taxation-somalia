c8bb56715b56eb8bfb24970a39b20e6ea8d2e5ee
MuzamilTahliil <muzamiltahliil1@gmail.com>
2025-05-17 19:03:02 +0300

    Update

--- routes/districtRoutes.js
+++ routes/districtRoutes.js
@@ -1,9 +1,27 @@
+// const express = require('express');
+// const router = express.Router();
+// const { getAllDistricts, createDistrict } = require('../controllers/districtController');
+
+// // Routes for districts
+// router.get('/', getAllDistricts);
+// router.post('/', createDistrict);
+
+// module.exports = router;
+
+
+
 const express = require('express');
 const router = express.Router();
-const { getAllDistricts, createDistrict } = require('../controllers/districtController');
+const {
+  getAllDistricts,
+  createDistrict,
+  updateDistrict,
+  deleteDistrict,
+} = require('../controllers/districtController');
 
-// Routes for districts
 router.get('/', getAllDistricts);
 router.post('/', createDistrict);
+router.put('/:id', updateDistrict);
+router.delete('/:id', deleteDistrict);
 
 module.exports = router;
