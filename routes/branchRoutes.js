aacdec44683e8493455eac4c5e57f380d730f5bb
MuzamilTahliil <muzamiltahliil1@gmail.com>
2025-05-17 19:02:15 +0300

    Update

--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -1,10 +1,41 @@
+// // // const express = require('express');
+// // // const router = express.Router();
+// // // const { getAllBranches, createBranch } = require('../controllers/branchController');
+
+// // // // Routes for branches
+// // // router.get('/', getAllBranches);
+// // // router.post('/', createBranch);
+
+// // // module.exports = router;
+
+
+
+
+
+// // const express = require('express');
+// // const router = express.Router();
+// // const { getAllBranches, createBranch } = require('../controllers/branchController');
+
+// // router.get('/', getAllBranches);
+// // router.post('/', createBranch);
+
+// // module.exports = router;
+
+
+
 // const express = require('express');
 // const router = express.Router();
-// const { getAllBranches, createBranch } = require('../controllers/branchController');
+// const {
+//   getAllBranches,
+//   createBranch,
+//   updateBranch,
+//   deleteBranch,
+// } = require('../controllers/branchController');
 
-// // Routes for branches
 // router.get('/', getAllBranches);
 // router.post('/', createBranch);
+// router.put('/:id', updateBranch);
+// router.delete('/:id', deleteBranch);
 
 // module.exports = router;
 
@@ -12,11 +43,40 @@
 
 
 
+
+// const express = require('express');
+// const router = express.Router();
+// const {
+//   getAllBranches,
+//   createBranch,
+//   updateBranch,
+//   deleteBranch,
+// } = require('../controllers/branchController');
+
+// router.get('/', getAllBranches);
+// router.post('/', createBranch);
+// router.put('/:id', updateBranch);
+// router.delete('/:id', deleteBranch);
+
+// module.exports = router;
+
+
+
+// routes/branchRoutes.js
 const express = require('express');
 const router = express.Router();
-const { getAllBranches, createBranch } = require('../controllers/branchController');
+const {
+  getAllBranches,
+  getBranchById,
+  createBranch,
+  updateBranch,
+  deleteBranch,
+} = require('../controllers/branchController');
 
 router.get('/', getAllBranches);
+router.get('/:id', getBranchById);  // Important for fetching one branch for editing
 router.post('/', createBranch);
+router.put('/:id', updateBranch);
+router.delete('/:id', deleteBranch);
 
 module.exports = router;
