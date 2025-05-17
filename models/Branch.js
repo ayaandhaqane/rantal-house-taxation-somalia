24dd2cef357bd796a431847bc8d379d5d5010f20
MuzamilTahliil <muzamiltahliil1@gmail.com>
2025-05-17 19:00:08 +0300

    Update

--- models/Branch.js
+++ models/Branch.js
@@ -13,6 +13,18 @@
 
 
 
+// const mongoose = require('mongoose');
+
+// const branchSchema = new mongoose.Schema({
+//   district_id: { type: mongoose.Schema.Types.ObjectId, ref: 'District', required: true },
+//   branch_name: { type: String, required: true }
+// });
+
+// const Branch = mongoose.model('Branch', branchSchema);
+// module.exports = Branch;
+
+
+
 const mongoose = require('mongoose');
 
 const branchSchema = new mongoose.Schema({
