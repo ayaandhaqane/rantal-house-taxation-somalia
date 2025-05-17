2ee52fe140a9d1dd3893b018fdd986c1333d55a1
MuzamilTahliil <muzamiltahliil1@gmail.com>
2025-05-17 19:03:20 +0300

    Update

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,15 +1,53 @@
+// // const express = require('express');
+// // const { createProperty, getAllProperties, getPropertyById } = require('../controllers/propertyController');
+
+// // const router = express.Router();
+
+// // // Route to create a new property
+// // router.post('/create', createProperty);
+
+// // // Route to get all properties
+// // router.get('/', getAllProperties);
+
+// // // Route to get a specific property by ID
+// // router.get('/:id', getPropertyById);
+
+// // module.exports = router;
+
+
+
+// const express = require('express');
+// const {
+//   createProperty,
+//   getAllProperties,
+//   getPropertyById,
+//   updateProperty,
+//   deleteProperty,
+// } = require('../controllers/propertyController');
+
+// const router = express.Router();
+
+// router.post('/create', createProperty);
+// router.get('/', getAllProperties);
+// router.get('/:id', getPropertyById);
+// router.put('/:id', updateProperty);
+// router.delete('/:id', deleteProperty);
+
+// module.exports = router;
+
+
+
 const express = require('express');
-const { createProperty, getAllProperties, getPropertyById } = require('../controllers/propertyController');
+const {
+  createProperty,
+  getAllProperties,
+  updateProperty,
+} = require('../controllers/propertyController');
 
 const router = express.Router();
 
-// Route to create a new property
 router.post('/create', createProperty);
-
-// Route to get all properties
 router.get('/', getAllProperties);
-
-// Route to get a specific property by ID
-router.get('/:id', getPropertyById);
+router.put('/:id', updateProperty);
 
 module.exports = router;
