5235277b7218f1fbca9b738799317ddeb207bd9b
MuzamilTahliil <muzamiltahliil1@gmail.com>
2025-05-17 19:00:46 +0300

    Update

--- models/District.js
+++ models/District.js
@@ -1,7 +1,17 @@
+// const mongoose = require('mongoose');
+
+// const districtSchema = new mongoose.Schema({
+//     district_name: { type: String, required: true }
+// });
+
+// const District = mongoose.model('District', districtSchema);
+// module.exports = District;
+
+
 const mongoose = require('mongoose');
 
 const districtSchema = new mongoose.Schema({
-    district_name: { type: String, required: true }
+  district_name: { type: String, required: true }
 });
 
 const District = mongoose.model('District', districtSchema);
