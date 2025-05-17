c49c5deca4ab443a738add03d9963d174b280d42
MuzamilTahliil <muzamiltahliil1@gmail.com>
2025-05-17 19:02:36 +0300

    Update

--- routes/citizenRoutes.js
+++ routes/citizenRoutes.js
@@ -1,12 +1,86 @@
+// // // const express = require('express');
+// // // const { createCitizen, getAllCitizens } = require('../controllers/citizenController');
+
+// // // const router = express.Router();
+
+// // // // POST route to create a new citizen
+// // // router.post('/create', createCitizen);
+
+// // // // GET route to get all citizens
+// // // router.get('/', getAllCitizens);
+
+// // // module.exports = router;
+
+
+
+// // const express = require('express');
+// // const { createCitizen, getAllCitizens } = require('../controllers/citizenController');
+
+// // const router = express.Router();
+
+// // // POST create citizen
+// // router.post('/create', createCitizen);
+
+// // // GET all citizens
+// // router.get('/', getAllCitizens);
+
+// // module.exports = router;
+
+
+
+// const express = require('express');
+// const {
+//   createCitizen,
+//   getAllCitizens,
+//   updateCitizen,
+//   deleteCitizen
+// } = require('../controllers/citizenController');
+
+// const router = express.Router();
+
+// router.post('/create', createCitizen);    // Create
+// router.get('/', getAllCitizens);          // Read all
+// router.put('/:id', updateCitizen);        // Update by ID
+// router.delete('/:id', deleteCitizen);     // Delete by ID
+
+// module.exports = router;
+
+
+
+// const express = require('express');
+// const { createCitizen, getAllCitizens } = require('../controllers/citizenController');
+
+// const router = express.Router();
+
+// // POST create citizen
+// router.post('/create', createCitizen);
+
+// // GET all citizens
+// router.get('/', getAllCitizens);
+
+// module.exports = router;
+
+
 const express = require('express');
-const { createCitizen, getAllCitizens } = require('../controllers/citizenController');
+const {
+  createCitizen,
+  getAllCitizens,
+  updateCitizen,
+  deleteCitizen,
+} = require('../controllers/citizenController');
 
 const router = express.Router();
 
-// POST route to create a new citizen
+// POST create citizen
 router.post('/create', createCitizen);
 
-// GET route to get all citizens
+// GET all citizens
 router.get('/', getAllCitizens);
+
+// PUT update citizen by ID
+router.put('/:id', updateCitizen);
+
+// DELETE citizen by ID
+router.delete('/:id', deleteCitizen);
 
 module.exports = router;
