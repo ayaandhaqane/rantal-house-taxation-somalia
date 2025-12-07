2e6b312379c02e23386eb2288cb8c31dd51f85b7
ayaandhaqane <148534224+ayaandhaqane@users.noreply.github.com>
2025-12-07 05:49:21 +0300

    firt commit

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,18 +1,4 @@
-// const express = require('express');
-// const { authenticate } = require('../middleware/authenticate');
 
-// const router = express.Router();
-
-// const { createTransaction,getTransactions, getTransactionsByPropertyId, getTransactionsByTaxCollectionStatus, getTransactionStats,transactionController, getTransactionsByDistrict  } = require('../controllers/transactionController');
-
-// router.get('/', getTransactions);  // Correct route definition
-// router.get('/property/:property_id', getTransactionsByPropertyId);
-// router.get('/status/:status', getTransactionsByTaxCollectionStatus);
-// router.get('/stats', getTransactionStats);
-
-
-
-// module.exports = router;
 
 const express = require('express');
 const { authenticate } = require('../middleware/authenticate');
